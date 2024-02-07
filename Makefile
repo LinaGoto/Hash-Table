5e55da1a53928e2f3e07af383ead007e2ddf5d66
Lina Goto <452215@bsd48.org>
2024-02-06 19:30:25 -0800

    adding gen student

--- Makefile
+++ Makefile
@@ -12,44 +12,11 @@ linkedlist.exe : main.o node.o student.o
 %.o : %.cpp node.h student.h
 	$(CC) $(CFLAGS) -c -o $@ $<
 
-scripts.txt : dummy
-	cp /dev/null $@
-	echo add >> $@
-	echo Lina Goto >> $@
-	echo 452215 >> $@
-	echo 3.9 >> $@
-	echo add >> $@
-	echo Hibiki Goto >> $@
-	echo 413343 >> $@
-	echo 3.2 >> $@
-	echo add >> $@
-	echo Kazushige Goto >> $@
-	echo 382413 >> $@
-	echo 3.6 >> $@
-	echo add >> $@
-	echo Ellie Goto >> $@
-	echo 463451 >> $@
-	echo 3.8 >> $@
-	echo Hoge Suzuki >> $@
-	echo 462215 >> $@
-	echo 3.1 >> $@
-	echo add >> $@
-	echo Kusa Suzuki >> $@
-	echo 411343 >> $@
-	echo 2.8 >> $@
-	echo add >> $@
-	echo Kawai Suzuki >> $@
-	echo 211343 >> $@
-	echo 2.4 >> $@
-	echo add >> $@
-	echo Bimyou Suzuki >> $@
-	echo 221343 >> $@
-	echo 2.6 >> $@
-	echo print >> $@
-	echo del >> $@
-	echo 211343 >> $@
-	echo print >> $@
-	echo quit >> $@
+scripts.txt : genstudent.exe
+	./genstudent.exe 150 > $@
+
+genstudent.exe : genstudent.cpp
+	$(CC) $(CFLAGS) -o $@ $^
 
 dummy :
 
---
+++ genstudent.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+#define	STUDENT_ID_MIN	100000
+#define	STUDENT_ID_MAX	999999
+
+#define NAME_LEN_MIN	 5
+#define NAME_LEN_MAX	10
+
+static int *id = NULL;
+
+static int genrandom(int min, int max) {
+  return (rand() % (max - min + 1)) + min;
+}
+
+static int getid(void) {
+  int newid;
+  do {
+    newid = genrandom(STUDENT_ID_MIN, STUDENT_ID_MAX);
+  } while (id[newid] != 0);
+  id[newid] = 1;
+  return newid;
+}
+
+static float getgpa(void) {
+  /* generate value : 0 to 400 */
+  return (float)genrandom(0, 400) / 100.;
+}
+
+static char firstname[NAME_LEN_MAX + 1];
+static char lastname [NAME_LEN_MAX + 1];
+
+static void genname(char *name) {
+  int namelen, i;
+  namelen = genrandom(NAME_LEN_MIN, NAME_LEN_MAX);
+
+  /* Fist letter is capital */
+  *name = 'A' + genrandom(0, 25);
+  name ++;
+  for (i = 1; i < namelen; i ++) {
+    *name = 'a' + genrandom(0, 25);
+    name ++;
+  }
+  *name = '\0';
+}
+
+int main(int argc, char **argv) {
+  int numstudent = 10;
+  int i;
+  
+  if (argc > 1) numstudent = atoi(argv[1]);
+
+  id = (int *)malloc((STUDENT_ID_MAX + 1) * sizeof(int));
+  if (id == (int *)NULL) exit(1);
+
+  for (i = 0; i < STUDENT_ID_MAX + 1; i ++) id[i] = 0;
+
+  for (i = 0; i < numstudent; i ++) {
+    printf("add\n");
+    genname(firstname);
+    genname(lastname);
+    printf("%s %s\n", firstname, lastname);
+    printf("%d\n", getid());
+    printf("%3.2f\n", getgpa());
+  }
+  
+  printf("print\n");
+  printf("quit\n");
+  
+  free(id);
+
+  return 0;
+}
